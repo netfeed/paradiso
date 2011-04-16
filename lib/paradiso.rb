0af8985f0b27027a111562df48fa08e6599d6539
Victor Bergöö <victor.bergoo@gmail.com>
2011-04-17 00:40:45 +0200

    moved the bin-file stuff into the bin-file

--- lib/paradiso.rb
+++ lib/paradiso.rb
@@ -30,17 +30,6 @@ module Paradiso
   }
   
   class << self
-    def run
-      options = config_file 
-      options, args = parse_cl!(options, ARGV)
-      
-      para = Paradiso.new options, args
-      para.run
-    rescue ArgumentError => e
-      puts "Error: #{e}"
-      exit 1
-    end
-    
     def config_file 
       options = Options.dup
       
@@ -51,53 +40,6 @@ module Paradiso
       end
 
       return options
-    end
-
-    def parse_cl! options, args
-      args.options do |o|
-        o.set_summary_indent '  '
-        o.banner = "Usage: #{File.basename $0} [Options]"
-        o.define_head "Paradiso #{VERSION}: A simple mplayer CLI"
-
-        o.on_tail("-h", "--help", "Show this help message.") { puts o; exit }
-        o.on_tail("-v", "--version", "Show version number") { puts "Paradiso %s" % [VERSION]; exit}
-
-        o.on("-a", "--amount n", Integer, "Play [n] items") { |amount|
-          options[:amount] = amount
-        }
-
-        o.on("-A", "--aspect-ratio n", String, "Aspect ratio - 16:9, 4:3..") { |ratio|
-          options[:aspectratio] = ratio
-        }
-
-        o.on("-d", "--delete", "Delete items from paylist") { 
-          options[:delete] = true
-        }
-
-        o.on("-f", "--fullscreen", "Fullscreen mode") { 
-          options[:fullscreen] = true
-        } 
-
-        o.on("-n", "--name path", String, "Playlist path to create") { |path|
-          options[:path] = path
-        }
-
-        o.on("-p", "--playlist", "The arguments is playlists") { 
-          options[:playlist] = true
-        }
-
-        o.on("-s", "--screen n", Integer, "Which screen to play from") { |screen|
-          options[:screen] = screen
-        }
-
-        o.on("-S", "--autosubs", "Load automatic subs") {
-          options[:autosubs] = true
-        }
-
-        o.parse!
-
-        return options, args
-      end
     end
   end
 end
